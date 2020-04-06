9491d1cfc1c3479f3ef2679221f23bee99c21114
Arsan sala <deh676@Arsans-MacBook-Air.local>
2020-04-07 00:29:53 +0700

    add cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,6 +80,10 @@ $app->configure('database');
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+ ]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use App\Http\Middleware\CorsMiddleware;
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
-
-$router->group(['prefix' => 'auth'], function () use ($router) {
-    $router->post('login', 'AuthenticationController@logIn');
-});
-$router->group(['prefix' => 'event'], function () use ($router) {
-    $router->get('get/list', 'EventController@getEventList');
-    $router->get('get/{id}', 'EventController@getEvent');
-    $router->post('add', 'EventController@addEvent');
-    $router->patch('edit/{id}', 'EventController@editEvent');
-    $router->patch('delete', 'EventController@deleteEvent');
+$router->group(['middleware' => [CorsMiddleware::class]], function () use ($router) {
+    $router->group(['prefix' => 'auth'], function () use ($router) {
+        $router->post('login', 'AuthenticationController@logIn');
+    });
+    $router->group(['prefix' => 'event'], function () use ($router) {
+        $router->get('get/list', 'EventController@getEventList');
+        $router->get('get/{id}', 'EventController@getEvent');
+        $router->post('add', 'EventController@addEvent');
+        $router->patch('edit/{id}', 'EventController@editEvent');
+        $router->patch('delete', 'EventController@deleteEvent');
+    });
 });
